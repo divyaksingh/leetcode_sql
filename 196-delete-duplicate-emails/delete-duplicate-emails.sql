197e4fb8a18d11a398452f3d2003c9d33a53b45d
divyaksingh <138142540+divyaksingh@users.noreply.github.com>
2024-05-23 22:31:34 -0400

    Time: 1275 ms (83.46%) | Memory: 0B (100.00%) - LeetSync

--- 196-delete-duplicate-emails/delete-duplicate-emails.sql
+++ 196-delete-duplicate-emails/delete-duplicate-emails.sql
@@ -1,3 +1,5 @@
 # Write your MySQL query statement below
-with cte as (select id, email , row_number() over (partition by email order by id) as rn from Person)
-delete from Person where id in (select id from cte where rn > 1)
+-- with cte as (select id, email , row_number() over (partition by email order by id) as rn from Person)
+-- delete from Person where id in (select id from cte where rn > 1)
+
+delete p2 from Person p1 inner join Person p2 on p1.email = p2.email where p1.id < p2.id
