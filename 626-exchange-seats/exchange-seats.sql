a7ac635f5499e95bdd5e27b5afd481003bd74b78
divyaksingh <138142540+divyaksingh@users.noreply.github.com>
2024-05-27 23:52:02 -0400

    Time: 634 ms (71.41%) | Memory: 0B (100.00%) - LeetSync

---
+++ 626-exchange-seats/exchange-seats.sql
@@ -0,0 +1,10 @@
+# Write your MySQL query statement below
+with cte as (select *, lead(id) over (order by id) next, lag (id) over (order by id) prev
+from Seat)
+
+select case when (id% 2 = 1 and next is not null) then next
+when (id% 2 = 0) then prev
+else id end as id,
+student 
+from cte
+order by id
