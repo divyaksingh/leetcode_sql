975ff8ab83ecd867426a69f52089ccbf6e99ff47
divyaksingh <138142540+divyaksingh@users.noreply.github.com>
2024-06-22 23:45:11 -0400

    Time: 366 ms (94.26%) | Memory: 0B (100.00%) - LeetSync

--- 197-rising-temperature/rising-temperature.sql
+++ 197-rising-temperature/rising-temperature.sql
@@ -1,8 +1,9 @@
 # Write your MySQL query statement below
+#Attempt2
 with cte as(select *, date_add(recordDate, interval-1 day) as YestDay, lag(recordDate) over (order by recordDate) as PrevDay, lag(temperature) over (order by recordDate) as PrevTemp from Weather)
-
 
 select id from cte where YestDay=PrevDay and temperature>PrevTemp
 
 
+#Attempt1
 -- select w1.id from Weather as w1, Weather as w2 where datediff(w1.recordDate, w2.recordDate) = 1 and w1.temperature > w2.temperature
